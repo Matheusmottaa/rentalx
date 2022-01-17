ba499d8f7f889c4b03916c6bd3df2d1a8bf406fe
Matheus Mota <matheusfelipe1918@gmail.com>
2022-01-17 20:06:24 -0300

    Adiciona arquivos ao projeto

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
+const app = express();
+const port: number = 3333;
+
+app.use(express.json());
+
+app.use('/categories', categoriesRoutes);
+
+app.listen(port, () => console.log('Server is running...')); 
