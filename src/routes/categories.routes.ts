bb39423de09c9fe2a18974ac61c6442aa918ca15
Matheus Mota <matheusfelipe1918@gmail.com>
2022-01-18 14:23:12 -0300

    verifica se a categoria ja existe

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -4,13 +4,20 @@ import { Category } from '../model/category';
 import { CategoriesRepository } from '../repositories/categoriesRepository';
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
-
 const categoriesRepository = new CategoriesRepository();
-categoriesRoutes.post("/", (request, response) => {
+
+categoriesRoutes.post('/', (request, response) => {
   const { name, description } = request.body;
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+  if (categoryAlreadyExists)
+    return response.status(400).json({ error: 'Category Already Exists' });
   categoriesRepository.create({ name, description });
   return response.status(201).json();
+});
+
+categoriesRoutes.get('/', (request, response) => {
+  const all = categoriesRepository.list();
+  return response.json(all);
 });
 
 export { categoriesRoutes }; 
