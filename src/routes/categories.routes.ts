7ee8875c9a9f8324eb65fbfcf167c6d1bc12f8c4
Matheus Mota <matheusfelipe1918@gmail.com>
2022-01-19 22:50:57 -0300

    removes the responsibility of creating a new category from routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,22 @@
-import { Router } from 'express';
-import { v4 as uuidv4 } from 'uuid';
-import { Category } from '../model/category';
-import { CategoriesRepository } from '../repositories/categoriesRepository';
+import { Router } from "express";
+
+import { CategoriesRepository } from "../repositories/categoriesRepository";
+import { CreateCategoryService } from "../services/createCategoryService";
+
 const categoriesRoutes = Router();
 
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post('/', (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-  if (categoryAlreadyExists)
-    return response.status(400).json({ error: 'Category Already Exists' });
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
   return response.status(201).json();
 });
 
-categoriesRoutes.get('/', (request, response) => {
+categoriesRoutes.get("/", (request, response) => {
   const all = categoriesRepository.list();
   return response.json(all);
 });
 
-export { categoriesRoutes }; 
+export { categoriesRoutes };
