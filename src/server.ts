0b9542b3093ce4ab17ae96fdb8b06559125da371
Matheus Mota <matheusfelipe1918@gmail.com>
2022-01-19 22:48:04 -0300

    update indentation

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
-import express from 'express';
-import { categoriesRoutes } from './routes/categories.routes';
+import express from "express";
+
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
-const port: number = 3333;
+const port = 3333;
 
 app.use(express.json());
 
-app.use('/categories', categoriesRoutes);
+app.use("/categories", categoriesRoutes);
 
-app.listen(port, () => console.log('Server is running...')); 
+app.listen(port, () => console.log("Server is running..."));
