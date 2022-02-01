ce0726270c78bc42ae163e4aacc3e3ef7555a779
Matheus Mota <matheusfelipe1918@gmail.com>
2022-02-01 19:50:09 -0300

    update server ts file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 
 import { categoriesRoutes } from "./routes/categories.routes";
+import { specificationRoutes } from "./routes/specifications.routes";
 
 const app = express();
 const port = 3333;
@@ -8,5 +9,7 @@ const port = 3333;
 app.use(express.json());
 
 app.use("/categories", categoriesRoutes);
+
+app.use("/specifications", specificationRoutes);
 
 app.listen(port, () => console.log("Server is running..."));
