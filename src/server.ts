a94ddf971a6110f48ce870e294557155fcf2e228
Matheus Mota <matheusfelipe1918@gmail.com>
2022-02-15 16:13:11 -0300

    update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,6 @@ import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
 const app = express();
-const port = 3333;
 
 app.use(express.json());
 
@@ -13,4 +12,4 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(port, () => console.log("Server is running..."));
+app.listen(3333, () => console.log("Server is running..."));
