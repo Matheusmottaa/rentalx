fd735ac09ee416bd1aea8e4237a36bb8b207fbcc
Matheus Mota <matheusfelipe1918@gmail.com>
2022-02-26 21:25:41 -0300

    update index

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -2,11 +2,11 @@ import { CategoriesRepository } from "../../repositories/implementations/categor
 import { CreateCategoryController } from "./CreateCategoryController";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-const categoriesRepository = CategoriesRepository.getInstance();
-const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
-
-const createCategoryController = new CreateCategoryController(
-  createCategoryUseCase,
-);
-
-export { createCategoryController, createCategoryUseCase };
+export default (): CreateCategoryController => {
+  const categoriesRepository = new CategoriesRepository();
+  const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+  const createCategoryController = new CreateCategoryController(
+    createCategoryUseCase,
+  );
+  return createCategoryController;
+};
