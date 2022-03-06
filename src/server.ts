aca842dea852c73a22e3637d0df8721a72917108
Matheus Mota <matheusfelipe1918@gmail.com>
2022-03-05 23:26:45 -0300

    update serve ts

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from "express";
+import "reflect-metadata";
 import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
-
-import "reflect-metadata";
 
 import "./database";
 
