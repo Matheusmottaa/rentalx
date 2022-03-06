01ea7d9db3421786d7cc9da604dada66751d5cc8
Matheus Mota <matheusfelipe1918@gmail.com>
2022-03-05 23:22:49 -0300

    add dependency injection with tsyringe

--- src/modules/cars/useCases/importCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -1,14 +1,14 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 
 import { ImportCategoryUseCase } from "./ImportCategoryUseCase";
 
 class ImportCategoryController {
-  constructor(private imporCategoryUseCase: ImportCategoryUseCase) {}
-
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { file } = request;
-    this.imporCategoryUseCase.execute(file);
-    return response.send();
+    const imporCategoryUseCase = container.resolve(ImportCategoryUseCase);
+    await imporCategoryUseCase.execute(file);
+    return response.status(201).send();
   }
 }
 
