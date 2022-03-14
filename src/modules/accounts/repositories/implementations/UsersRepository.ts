20a39c67d1523f947f5a67f67354294cfccbc04c
Matheusmottaa <matheusfelipe1918@gmail.com>
2022-03-13 23:06:35 -0300

    add findById on user repository

--- src/modules/accounts/repositories/implementations/UsersRepository.ts
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -16,18 +16,27 @@ class UsersRepository implements IUsersRepository {
     email,
     driver_license,
     password,
+    avatar,
+    id,
   }: ICreateUserDTO): Promise<void> {
     const user = this.repository.create({
       name,
       email,
       driver_license,
       password,
+      avatar,
+      id,
     });
     await this.repository.save(user);
   }
 
   async findByEmail(email: string): Promise<User> {
     const user = await this.repository.findOne({ email });
+    return user;
+  }
+
+  async findById(user_id: string): Promise<User> {
+    const user = await this.repository.findOne(user_id);
     return user;
   }
 }
