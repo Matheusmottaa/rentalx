223fcc275d4faaeec0fe6068c5b48772cceb58bb
Matheusmottaa <matheusfelipe1918@gmail.com>
2022-03-29 21:33:33 -0300

    add middleware authenticate admin user

--- src/shared/infra/http/routes/specifications.routes.ts
+++ src/shared/infra/http/routes/specifications.routes.ts
@@ -3,12 +3,17 @@ import { Router } from "express";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 import { CreateSpecificationController } from "../../../../modules/cars/useCases/createSpecification/CreateSpecificationController";
+import { ensureAdmin } from "../middlewares/ensureAdmin";
 
 const specificationRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
 specificationRoutes.use(ensureAuthenticated);
-specificationRoutes.post("/", createSpecificationController.handle);
+specificationRoutes.post(
+  "/",
+  ensureAdmin,
+  createSpecificationController.handle,
+);
 
 export { specificationRoutes };
