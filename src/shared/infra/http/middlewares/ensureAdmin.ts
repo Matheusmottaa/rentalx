ca0257d663b05d99259817dc3796025534a43a41
Matheusmottaa <matheusfelipe1918@gmail.com>
2022-03-29 21:31:14 -0300

    add authenticate admin user

---
+++ src/shared/infra/http/middlewares/ensureAdmin.ts
@@ -0,0 +1,18 @@
+import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import { Request, Response, NextFunction } from "express";
+
+import { AppError } from "@shared/Errors/AppError";
+
+export async function ensureAdmin(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const { id } = request.user;
+  const usersRepository = new UsersRepository();
+  const user = await usersRepository.findById(id);
+  if (!user.isAdmin) {
+    throw new AppError("User isn't an admin!");
+  }
+  return next();
+}
