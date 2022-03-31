3b7ac4dc517d24e10d10807ed816cfc7b71262f5
Matheusmottaa <matheusfelipe1918@gmail.com>
2022-03-30 22:25:59 -0300

    add specifications reporistory in memory

---
+++ src/modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory.ts
@@ -0,0 +1,30 @@
+import { Specification } from "@modules/cars/infra/typeorm/entities/specification";
+
+import {
+  ICreateSpecificationDTO,
+  ISpecificationRepostiroy,
+} from "../ISpecificationRepository";
+
+class SpecificationRepositoryInMemory implements ISpecificationRepostiroy {
+  specifications: Specification[] = [];
+
+  async create({ name, description }: ICreateSpecificationDTO): Promise<void> {
+    const specification = new Specification();
+    Object.assign(specification, {
+      description,
+      name,
+    });
+    this.specifications.push(specification);
+  }
+  async findByName(name: string): Promise<Specification> {
+    return this.specifications.find(specifcation => specifcation.name === name);
+  }
+  async findByIds(ids: string[]): Promise<Specification[]> {
+    const allSpecifications = this.specifications.filter(specification =>
+      ids.includes(specification.id),
+    );
+    return allSpecifications;
+  }
+}
+
+export { SpecificationRepositoryInMemory };
