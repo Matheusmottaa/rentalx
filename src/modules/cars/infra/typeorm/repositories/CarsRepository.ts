413d4a23e04e84f2ea081c0fcd6ec60645e45585
Matheusmottaa <matheusfelipe1918@gmail.com>
2022-03-30 22:33:14 -0300

    add query of available cars

--- src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CarsRepository.ts
@@ -19,6 +19,7 @@ class CarsRepository implements ICarsRepository {
     fine_amount,
     license_plate,
     name,
+    specifications,
   }: ICreateCarDTO): Promise<Car> {
     const car = this.repository.create({
       brand,
@@ -34,6 +35,30 @@ class CarsRepository implements ICarsRepository {
   }
   async findByLicensePlate(license_plate: string): Promise<Car> {
     const car = await this.repository.findOne({ license_plate });
+    return car;
+  }
+  async findAvailable(
+    category_id?: string,
+    brand?: string,
+    name?: string,
+  ): Promise<Car[]> {
+    const carsQuery = this.repository
+      .createQueryBuilder("c")
+      .where("available = :available", { available: true });
+    if (brand) {
+      carsQuery.andWhere("brand = :brand", { brand });
+    }
+    if (name) {
+      carsQuery.andWhere("name = :name", { name });
+    }
+    if (category_id) {
+      carsQuery.andWhere("category_id = :category_id", { category_id });
+    }
+    const cars = await carsQuery.getMany();
+    return cars;
+  }
+  async findById(id: string): Promise<Car> {
+    const car = await this.repository.findOne(id);
     return car;
   }
 }
