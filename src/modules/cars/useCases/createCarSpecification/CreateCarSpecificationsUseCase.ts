d1b32725dcd2678da4f1d0c2ee4af2de2823c42e
Matheusmottaa <matheusfelipe1918@gmail.com>
2022-03-31 20:07:25 -0300

    add create car specification usecase

---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecificationsUseCase.ts
@@ -0,0 +1,35 @@
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
+import { ISpecificationRepostiroy } from "@modules/cars/repositories/ISpecificationRepository";
+import { inject, injectable } from "tsyringe";
+
+import { AppError } from "@shared/Errors/AppError";
+
+interface IRequest {
+  car_id: string;
+  specifications_id: string[];
+}
+@injectable()
+class CreateCarSpecificationUseCase {
+  constructor(
+    @inject("CarsRepository")
+    private carsRepository: ICarsRepository,
+    @inject("SpecificationsRepository")
+    private specificationsRepository: ISpecificationRepostiroy,
+  ) {}
+
+  async execute({ car_id, specifications_id }: IRequest): Promise<Car> {
+    const carExists = await this.carsRepository.findById(car_id);
+    if (!carExists) {
+      throw new AppError("Car doesn't exists!");
+    }
+    const specifications = await this.specificationsRepository.findByIds(
+      specifications_id,
+    );
+    carExists.specifications = specifications;
+    await this.carsRepository.create(carExists);
+    return carExists;
+  }
+}
+
+export { CreateCarSpecificationUseCase };
