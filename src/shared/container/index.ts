d43c253ab32db85fca38e604f14f2070987770a3
Matheusmottaa <matheusfelipe1918@gmail.com>
2022-03-31 20:04:33 -0300

    register Cars repository as singleton pattern

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,10 @@
 import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { CarsImagesRepository } from "@modules/cars/infra/typeorm/repositories/CarsImageRepository";
 import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarsRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/categoriesRepository";
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/specificationsRepository";
+import { ICarsImageRepository } from "@modules/cars/repositories/ICarsImageRepository";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repositories/ICategoriesRepository";
 import { ISpecificationRepostiroy } from "@modules/cars/repositories/ISpecificationRepository";
@@ -24,3 +26,8 @@ container.registerSingleton<IUsersRepository>(
 );
 
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
+
+container.registerSingleton<ICarsImageRepository>(
+  "CarsImagesRepository",
+  CarsImagesRepository,
+);
